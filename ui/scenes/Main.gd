2509c5465277daf2e565c3bc58d87d890132d4cf
tilpner <till@hoeppner.ws>
2019-04-05 23:55:26 +0200

    Add ui

---
+++ ui/scenes/Main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+const libpolitsim = preload("res://lib/politsim.gdns")
+onready var PolitSim = libpolitsim.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(PolitSim.get_data())
