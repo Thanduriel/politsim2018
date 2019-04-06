5754433c9ad54e79a0615f6ff37a06660b1102de
Julian Benda <Zabuza98@gmx.net>
2019-04-06 18:56:01 +0200

    Adde Buttons to gui

--- ui/scenes/main.gd
+++ ui/scenes/main.gd
@@ -1,8 +1,9 @@
 extends Node
 
 const libpolitsim = preload("res://lib/politsim.gdns")
-onready var PolitSim = libpolitsim.new()
+# onready var PolitSim = libpolitsim.new()
 
 func _ready():
-	PolitSim.init(42)
-	print(PolitSim.World_Test())
+	print("hallo");
+	# PolitSim.init(42)
+	# print(PolitSim.World_Test())
