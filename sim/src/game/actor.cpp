839cdaf3a5917f12ed31f8adcc97bd92cc9ae4a7
tilpner <till@hoeppner.ws>
2019-04-07 13:56:29 +0200

    Fix syntax for gcc

--- sim/src/game/actor.cpp
+++ sim/src/game/actor.cpp
@@ -7,19 +7,19 @@ namespace Game {
 	using HobbyT = Map::Tile::Info::Hobby;
 
 	const PopulationClass LOWER_CLASS = {
-		{Quality::Low},
+		Quality::Low,
 		{1,2},
 		{HobbyT::Skate}
 	};
 
 	const PopulationClass MIDDLE_CLASS = {
-		{Quality::Mid},
+		Quality::Mid,
 		{3,4},
 		{HobbyT::Arcade, HobbyT::Tennis}
 	};
 
 	const PopulationClass UPPER_CLASS = {
-		{Quality::High},
+		Quality::High,
 		{5,6},
 		{HobbyT::Theater, HobbyT::Tennis}
 	};
