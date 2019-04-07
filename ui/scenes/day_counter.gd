31a060d392914cc9fa30e84bc144d9071acaeeeb
tilpner <till@hoeppner.ws>
2019-04-07 14:06:52 +0200

    Add clock

--- ui/scenes/day_counter.gd
+++ ui/scenes/day_counter.gd
@@ -3,6 +3,10 @@ extends Label
 onready var global = $"/root/global"
 
 func _process(delta):
+	var time = global.World.get_time()
 	var day = global.World.get_day()
 	var week = day / 7
-	text = "Day " + str(day) + ", KW " + str(week)
+	var hour = time * 24
+	var minute = int(hour * 60) % 60
+	text = ("%02d" % int(hour)) + ":" + ("%02d" % minute) + "\n" \
+		+ "Day " + str(day) + ", KW " + str(week)
