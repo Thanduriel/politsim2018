b29076d07d0e2a0c58b310ec08b38275f56284c5
tilpner <till@hoeppner.ws>
2019-04-07 17:41:48 +0200

    Add flier impl

--- sim/src/game/event.cpp
+++ sim/src/game/event.cpp
@@ -9,5 +9,12 @@ namespace Game{
 			std::cout << "change: " << change << "\n";
 			_actor.politic = std::clamp(_actor.politic + change, 0.f, 1.f);
 		}
+
+		void Flier::operator()(Actor& _actor, float _deltaTime) const
+		{
+			const float change = _deltaTime * m_influence;
+			if (_actor.politic < 0.5) _actor.politic = std::clamp(_actor.politic + change, 0.f, 0.5f);
+		}
+
 }
 }
