050b8a90983b3980d0270592800c2bdc79b477d5
Unknown <smaragdeule@gmx.de>
2019-04-07 15:34:43 +0200

    added sound manager
    again

--- ui/scenes/button.gd
+++ ui/scenes/button.gd
@@ -2,9 +2,11 @@ extends Button
 
 onready var tile_map = get_node("/root/main/tiles")
 onready var cursor = get_node("/root/main/cursor")
+onready var player = get_node("/root/main/sound/Button")
 
 export var action_type = "test"
 
 func pressed():
 	tile_map.send_action(action_type)
 	cursor.set_texture($"./CenterContainer/TextureRect".get_texture())
+	player.play()
